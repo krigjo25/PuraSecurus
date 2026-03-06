51578e295b0a525a3d546ef8c29afaa7bcd2415d
krigjo25 <krigjo25@gmail.com>
2026-03-06 11:27:15 +0100

    refactor(Config): 🚚 Moved `rxconfig.py` out of puraSecuras folder.

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,8 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="frontend",
-    plugins=[
+    app_name = "puraSecurus",
+    plugins = [
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
