0f81712e76ea0ff7273a3783d922953ea5ea24d0
krigjo25 <krigjo25@gmail.com>
2026-03-06 13:19:55 +0100

    refactor(UI): ♻️ Implement `layout_wrapper`  -> Encapsulates Headers, Main & Footers.     -   Create a shared layout template to enforce DRY principles across     all pages, this centralizes the structure, making it easier to maintain     and update the overall design of the application.

--- puraSecurus/puraSecurus.py
+++ puraSecurus/puraSecurus.py
@@ -1,27 +1,36 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
+#   Third-party Libraries
 import reflex as rx
-
 from rxconfig import config
+
+#   Internal Libraries
+from components.template.header import header
 
 
 class State(rx.State):
     """The app state."""
 
+def layout_wrapper(page_content: rx.Component) -> rx.Component:
+    return rx.fragment(                                                             #type: ignore - Fragment is a valid component, but pylance doesn't recognize it.)
+    rx.color_mode.button(position="top-right"),                                     #type: ignore - ColorModeButton is a valid component, but pylance doesn't recognize it.
+    
+    header(),
+    
+    rx.container(page_content),                                                     #type: ignore - Container is a valid component, but pylance doesn't recognize it.
+
+    #   footer()
+    )
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+
+    main:rx.Component = rx.container(                                               #type: ignore - Container is a valid component, but pylance doesn't recognize it.
+        rx.vstack(                                                                  #type: ignore - VStack is a valid component, but pylance doesn't recognize it.
             rx.text(
                 "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                rx.code(f"{config.app_name}/{config.app_name}.py"),                 #type: ignore - Text and Code are valid components, but pylance doesn't recognize them.
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
+            rx.link(                                                                #type: ignore - Link is a valid component, but pylance doesn't recognize it.
+                rx.button("Check out our docs!"),                                   #type: ignore - Button is a valid component, but pylance doesn't recognize it.
                 href="https://reflex.dev/docs/getting-started/introduction/",
                 is_external=True,
             ),
@@ -30,7 +39,17 @@ def index() -> rx.Component:
             min_height="85vh",
         ),
     )
+    return layout_wrapper(page_content = main)
 
+def add_resturant() -> rx.Component:
+    main: rx.Component = rx.text("Add Resturant Page")                              #type: ignore - Text is a valid component, but pylance doesn't recognize it.
+    return layout_wrapper(page_content=main)
+
+def view_details() -> rx.Component:
+    main: rx.Component = rx.text("View Details Page")                               #type: ignore - Text is a valid component, but pylance doesn't recognize it.
+    return layout_wrapper(page_content=main)
 
 app = rx.App()
-app.add_page(index)
+app.add_page(index)                                                                  #type: ignore - add_page is a valid method, but pylance doesn't recognize it.
+app.add_page(add_resturant)                                                      #type: ignore - add_resturant is not defined, but it is a valid page that will be added later.
+app.add_page(view_details)                                                       #type: ignore - view_details is not defined, but it is a valid page that will be added later.
