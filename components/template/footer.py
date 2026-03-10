2506a83c2f178d6434dfce3a9d3643e9015e6e64
krigjo25 <krigjo25@gmail.com>
2026-03-10 11:08:07 +0100

    style: 💄 Removed spaceing

--- components/template/footer.py
+++ components/template/footer.py
@@ -3,4 +3,4 @@ import reflex as rx
 
 def copyright() -> rx.Component: return rx.text("© 2026 PuraSecurus. All rights reserved.", size="1", color="gray.500")    #type: ignore - Text is a valid component, but pylance doesn't recognize it.
 def footer() -> rx.Component:
-    return rx.hstack( copyright())                                                                                          #type: ignore - HStack is a valid component, but pylance doesn't recognize it.
+    return rx.hstack(copyright())                                                                                          #type: ignore - HStack is a valid component, but pylance doesn't recognize it.
