5485bb27d6a3c99a9bfe5efe7c062f970b33bdfd
krigjo25 <krigjo25@gmail.com>
2026-03-10 11:16:37 +0100

    refactor(UI): Implement txconfig & Improve Semantic Layout Structure.

--- components/template/header.py
+++ components/template/header.py
@@ -1,23 +1,31 @@
-#   Third-party Libraries
+# Standard Libraries
 from typing import Dict, List
 
+#   Third-party Libraries
 import reflex as rx
+from rxconfig import config
 
 #   Internal Libraries
 from components.text.elements import heading
 from components.navigation.navigation import anchor_navigation
 
-def logo() -> rx.Component: return rx.image(src="logo/logo.png", alt="PuraSecurus Logo", width="50px")                  #type: ignore - Image is a valid component, but pylance doesn't recognize it.
+def logo() -> rx.Component: return rx.image(src="logo/logo.png", alt=f"{config.app_name} Logo", width="50px")       #type: ignore - Image is a valid component, but pylance doesn't recognize it.
 
 def header() -> rx.Component:
 
     raw_menu_items: List[Dict[str, str | List[str]]] = [
         { "label": "List of Resturants","href": "/", "cls": ["btn", "router-btn"] },
         { "label": "Add Resturant", "href": "/add-resturant", "cls": ["btn", "router-btn"] },
-        { "label": "View Details", "href": "/view-details", "cls": ["btn", "router-btn"] },
     ]
-    return rx.hstack(                                                                                                   #type: ignore - HStack is a valid component, but pylance doesn't recognize it.                               
-        logo(),
-        heading("PuraSecurus", size="9"),
-        anchor_navigation(raw_menu_items),
-        )
+
+
+    return rx.box(                                                                                                  #type: ignore - Box is a valid component, but pylance doesn't recognize it.
+            rx.vstack(                                                                                              #type: ignore - HStack is a valid component, but pylance doesn't recognize it.
+                logo(),
+                heading(config.app_name, size="9")
+                ),
+
+            rx.hstack(                                                                                          #type: ignore - VStack is a valid component, but pylance doesn't recognize it.
+                    anchor_navigation(raw_menu_items)
+                ),
+                as_="header"),
